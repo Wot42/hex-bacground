393a7ebd40830ff251a0f04609b24c3ddec0ad23
Wot Fanar <wotfanar.webdeveloper@gmail.com>
2023-03-27 21:58:12 +0100

    drawn hex grid

--- hexScript.js
+++ hexScript.js
@@ -3,14 +3,16 @@ const ctxH = canvasHex.getContext('2d');
 ctxH.canvas.width = window.innerWidth;
 ctxH.canvas.height = window.innerHeight;
 
-let hexArray = [];
-const maxSize = 100;
+var hexArray = [];
+const maxSize = 40;
 const minSize = 0;
 const maxMouseRadius = 100;
 const minMouseRadius = 300;
 const rootThree = Math.sqrt(3);
+var rowHex = null;
+var columnHex = null;
 
-let mouse = {
+var mouse = {
   x: null,
   y: null
 };
@@ -37,5 +39,23 @@ class Hex {
   }
 }
 
-let testHex = new Hex(100,100,50);
-testHex.draw();
+var setUp = () => {
+  hexArray = [];
+  rowHex = Math.floor(window.innerWidth/(maxSize*3))+2;
+  columnHex = Math.floor(window.innerHeight/(maxSize*rootThree/2))+2;
+  var x = 0;
+  var y = 0;
+
+  for (var r = 0; r < rowHex; r++){
+    for (var c = 0; c < columnHex; c++){
+      x = r*maxSize*3 + ((c%2 == 0) ? 0 : (maxSize*1.5));
+      y = c*maxSize*rootThree/2;
+      hexArray.push(new Hex(x,y,maxSize-1));
+    }
+  }
+  hexArray.forEach(hex => {hex.draw();});
+}
+
+// var testHex = new Hex(100,100,50);
+// testHex.draw();
+setUp();
